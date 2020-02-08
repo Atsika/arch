ce3d1a5b1f6171c29cf7ca073d7287d63c09cc91
atsika <micronesic@live.fr>
2020-02-08 18:54:01 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -56,3 +56,21 @@ pacstrap /mnt base linux linux-firmware
 genfstab -U /mnt >> /mnt/etc/fstab
 
 arch-chroot /mnt
+
+ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
+
+hwclock --systohc
+
+locale-gen
+
+echo "LANG=fr_FR.UTF-8" >> /etc/locale.conf
+
+echo "KEYMAP=fr" >> /etc/vconsole.conf
+
+echo "linux" >> /etc/hostname
+
+echo "127.0.0.1		localhost" >> /etc/hosts
+echo "::1			localhost" >> /etc/hosts
+echo "127.0.1.1		linux.localdomain	linux" >> /etc/hosts
+
+echo -e "root\nroot" | (passwd root)
