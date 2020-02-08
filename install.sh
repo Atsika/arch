376e0327a67feb551c0b8f849e34b34c820c2693
atsika <micronesic@live.fr>
2020-02-08 16:55:45 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+# get disks
+disksize=$(fdisk -l | grep /dev/sd* | awk -F " " {'print $5'})
+echo "Disk size : $disksize"
+
 # get ram size
 ram=$(free --si | grep Mem | awk -F " " {'print $2'})
-echo $ram
+echo "Total ram : $ram"
