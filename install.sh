2cd73a51a96faade28a85159617441b0f14201ce
atsika <micronesic@live.fr>
2020-02-08 19:17:52 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -51,7 +51,7 @@ swapon "${diskname}2"
 
 mount "${diskname}1" /mnt
 
-pacstrap /mnt base linux linux-firmware
+pacstrap /mnt base
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
@@ -74,5 +74,11 @@ echo "::1		localhost" >> /etc/hosts
 echo "127.0.1.1		linux.localdomain	linux" >> /etc/hosts
 
 echo -e "root\nroot" | (passwd root)
+
+pacman -S grub
+
+grub-install "${diskname}1"
+
+grub-mkconfig -o /boot/grub/grub.cfg
 
 EOC
