27827d321d9429068666d6c7c3d9f0ffbfacd3af
atsika <micronesic@live.fr>
2020-02-08 19:40:26 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -51,34 +51,6 @@ swapon "${diskname}2"
 
 mount "${diskname}1" /mnt
 
-pacstrap /mnt base
+pacstrap /mnt base linux linux-firmawre
 
 genfstab -U /mnt >> /mnt/etc/fstab
-
-arch-chroot /mnt /bin/bash << EOC
-
-ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
-
-hwclock --systohc
-
-locale-gen
-
-echo "LANG=fr_FR.UTF-8" >> /etc/locale.conf
-
-echo "KEYMAP=fr" >> /etc/vconsole.conf
-
-echo "linux" >> /etc/hostname
-
-echo "127.0.0.1		localhost" >> /etc/hosts
-echo "::1		localhost" >> /etc/hosts
-echo "127.0.1.1		linux.localdomain	linux" >> /etc/hosts
-
-echo -e "root\nroot" | (passwd root)
-
-pacman -S grub
-
-grub-install "${diskname}1"
-
-grub-mkconfig -o /boot/grub/grub.cfg
-
-EOC
