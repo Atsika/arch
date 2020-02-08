05aac20e66e53f8178bff59717bff8a5d155f14f
atsika <micronesic@live.fr>
2020-02-08 20:36:26 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,7 +24,6 @@ swapsize=$(free --si --mega | grep Mem | awk -F " " {'print $2'})
 
 ext4size=$(($disksize-$swapsize))
 
-
 fdisk $diskname << FDISK
 o
 n
@@ -43,6 +42,8 @@ t
 t
 2
 82
+a
+1
 w
 FDISK
 
@@ -53,6 +54,36 @@ swapon "${diskname}2"
 
 mount "${diskname}1" /mnt
 
-pacstrap /mnt base linux linux-firmware
+pacstrap /mnt base linux linux-firmawre
 
 genfstab -U /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnt /bin/bash << EOC
+
+ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
+
+hwclock --systohc
+
+locale-gen
+
+echo "LANG=fr_FR.UTF-8" >> /etc/locale.conf
+
+export LANG=fr_FR.UTF-8
+
+echo "KEYMAP=fr" >> /etc/vconsole.conf
+
+echo "linux" >> /etc/hostname
+
+echo "127.0.0.1		localhost" >> /etc/hosts
+echo "::1			localhost" >> /etc/hosts
+echo "127.0.1.1		linux.localdomain	linux" >> /etc/hosts
+
+echo -e "root\nroot" | (passwd root)
+
+pacman -S grub
+
+grub-install "${diskname}1"
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
+EOC
