f0d28da48b5d56100c8c411097b360f9019959d3
atsika <micronesic@live.fr>
2020-02-08 18:18:56 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,10 +18,10 @@ diskname="${diskname//:}"
 
 # get disk size
 disksize=$(fdisk -l | grep $diskname | awk -F " " {'print $5'})
-echo "$(($disksize/(2^20)))"
+echo "$(($disksize/1048576))"
 # get ram size
 swapsize=$(free --si | grep Mem | awk -F " " {'print $2'})
-echo "$(($swapsize/(2^20)))"
+echo "$(($swapsize/1048576))"
 
 ext4size=$(($disksize-$swapsize))
 
