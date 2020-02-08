7ce70bc7fe23fbe948471ff62ff68b7636afdf59
atsika <micronesic@live.fr>
2020-02-08 18:54:33 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -70,7 +70,7 @@ echo "KEYMAP=fr" >> /etc/vconsole.conf
 echo "linux" >> /etc/hostname
 
 echo "127.0.0.1		localhost" >> /etc/hosts
-echo "::1			localhost" >> /etc/hosts
+echo "::1		localhost" >> /etc/hosts
 echo "127.0.1.1		linux.localdomain	linux" >> /etc/hosts
 
 echo -e "root\nroot" | (passwd root)
