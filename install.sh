4781b401e88e0809f8a7dfc66f1740a93026f5d2
atsika <micronesic@live.fr>
2020-02-08 17:41:26 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ timedatectl set-ntp true
 diskname=$(fdisk -l | grep /dev/sd* | awk -F " " {'print $2'})
 
 # get disk size
-disksize=$(fdisk -l | grep /dev/$((diskname)) | awk -F " " {'print $5'})
+disksize=$(fdisk -l | grep /dev/${diskname//:} | awk -F " " {'print $5'})
 
 # get ram size
 ramsize=$(free --si | grep Mem | awk -F " " {'print $2'})
