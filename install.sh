15fac9981205f84ca668f2a354e7eb9059231706
atsika <micronesic@live.fr>
2020-02-08 17:06:05 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 
+if [[ ! -f "/sys/firmware/efi/efivars" ]]
+then
+	echo "BIOS"
+else
+	echo "UEFI"
+fi
+
 # get disks
 disksize=$(fdisk -l | grep /dev/sd* | awk -F " " {'print $5'})
 echo "Disk size : $disksize"
